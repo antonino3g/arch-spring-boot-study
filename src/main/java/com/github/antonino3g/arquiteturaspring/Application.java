5054b803a854a71e9d568af1f001e4849c394af1
Antonino <antonino.praxedes@gmail.com>
2025-05-14 23:39:53 -0300

    Entendendo a classe Application

---
+++ src/main/java/com/github/antonino3g/arquiteturaspring/Application.java
@@ -0,0 +1,18 @@
+package com.github.antonino3g.arquiteturaspring;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+//        SpringApplication.run(Application.class, args);
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(Application.class);
+        builder.run(args);
+        builder.bannerMode(Banner.Mode.OFF);
+
+    }
+}
