c588ae28d385eb2b3964b1e378f48cf7b4af417a
Antonino <antonino.praxedes@gmail.com>
2025-05-20 22:59:59 -0300

    using annotation

---
+++ src/main/java/com/github/antonino3g/arquiteturaspring/api/Turbo.java
@@ -0,0 +1,15 @@
+package com.github.antonino3g.arquiteturaspring.api;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.METHOD})
+@Qualifier("motorEletrico")
+public @interface Turbo {
+
+}
